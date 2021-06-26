97b20a297cc97ff70d1c5abaee82e04f30623a29
Shiimii <atau0435no2@gmail.com>
2021-06-26 15:42:51 +0900

    remove then in rb

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,9 +1,9 @@
 for num in 1..100 do
-    if num % 15 == 0 then
+    if num % 15 == 0
         puts "FizzBuzz"
-    elsif num % 3 == 0 then
+    elsif num % 3 == 0
         puts "Fizz"
-    elsif num % 5 == 0 then
+    elsif num % 5 == 0
         puts "Buzz"
     else
         puts num
